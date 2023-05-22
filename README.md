a57864ee4af28da3944f9d0521e49bbd2f2109a9
Louis Sullivan <sullivanlouis0@gmail.com>
2023-05-22 16:53:03 +0100

    added setupConfig/connect to db

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Scrap some food data for SteW
+	// c.OnRequest(func(r *colly.Request) {
+	// 	fmt.Println("Logger", "Visiting: ", r.URL)
+	// })
+
+	// c.OnError(func(_ *colly.Response, err error) {
+	// 	fmt.Println("Logger", "Error: ", err)
+	// })
+
+	// c.OnResponse(func(r *colly.Response) {
+	// 	fmt.Println("Logger", "Visit Complete: ", r.Request.URL)
+	// })
+
+	// c.OnScraped(func(r *colly.Response) { 
+	// 	fmt.Println("Logger", "Scrape Complete: ", r.Request.URL)
+	// })
--- main.go
+++ main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"regexp"
-	"strconv"
+	"io/ioutil"
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
 type FoodItem struct{
+	fid int
 	name, serving, calories string
 }
 
+type Config struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Dbname   string `json:"dbname"`
+}
+
 func main() {
+	// set config values from db.json 
+	config, _ := setupConfig()
+
 	foodArray := scrapeFood()
-	fmt.Println(foodArray)	
+	if len(foodArray) < 1 {
+		fmt.Println("Error scraping food items")
+	} else {
+		// fmt.Println(foodArray)
+		dbDone := insertDb(foodArray, config)
+		if (dbDone) {
+			fmt.Println("Database updated")
+		} else {
+			fmt.Println("Error updating database")
+		}
+	}
 }
 
 func scrapeFood() []FoodItem {
@@ -29,19 +53,48 @@ func scrapeFood() []FoodItem {
 	"venison-game", "wine", "yogurt"}
 
 	var foodArray []FoodItem
+	countFid := 0
 
 	for i := 0; i < len(categoriesArray); i++ {
 		var foodItem FoodItem
 		c.OnHTML("tr", func(e *colly.HTMLElement){
 			// skip the title values
 			if ((e.ChildText("td.serving")) != "ServingServingServing") {
+				// convert int to string
+				foodItem.fid = countFid
 				foodItem.name = e.ChildText("a")
 				foodItem.serving = "100"
-				foodItem.calories = e.ChildText("td.kcal")
+				cals := e.ChildText("td.kcal")
+				foodItem.calories = strings.Split(cals, "cal")[0]
 				foodArray = append(foodArray, foodItem)
+				countFid++
 			}
 		})
 		c.Visit(foodURL + categoriesArray[i])
 	}
 	return foodArray
+}
+
+func insertDb(foodArray []FoodItem, config Config) bool {
+	// Table created: CREATE table StewFood ( Fid INT PRIMARY KEY, Name TEXT, Serving TEXT, Calories TEXT,);
+	// read in const from db.txt
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", config.Host, config.Port, config.User, config.Password, config.Dbname)
+	fmt.Println(psqlconn)
+	return true
+}
+
+func setupConfig() (Config, error) {
+	var config Config
+
+	data, err := ioutil.ReadFile("db.json")
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
